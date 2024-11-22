4d7e747ff70d4d73f5ae5409379b6b6fbb7db71f
talelat <64014195@rbac.ac.th>
2024-11-22 12:13:57 +0700

    commit1

---
+++ me.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <p>My name is talelat</p>
+      <p>My nickname is milk</p>
+    </div>
+  );
+};
+
+export default MyComponent;
---
+++ menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <p>Hello</p>
+      <p>Product</p>
+      <p>About me</p>
+      <p>123</p>
+    </div>
+  );
+};
+
+export default MyComponent;
